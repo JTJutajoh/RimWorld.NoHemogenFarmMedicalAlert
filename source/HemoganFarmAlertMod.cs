db1cd7b497fa9b1fb40de399a825663fba56751f
Justin <darkflame57@gmail.com>
2022-11-10 20:01:49 -0800

    Initial build

---
+++ source/HemoganFarmAlertMod.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+
+namespace NoHemogenFarmMedicalAlert
+{
+    public class HemoganFarmAlertMod : Verse.Mod
+    {
+        public HemoganFarmAlertMod(ModContentPack content) : base(content)
+        {
+            //Get settings
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    static class LoadHarmony
+    {
+        static LoadHarmony()
+        {
+            Harmony harmony = new Harmony("Dark.NoHemogenFarmMedicalAlert");
+
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ source/Patch_Alert_LifeThreateningHediff.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace NoHemogenFarmMedicalAlert
+{
+    [HarmonyPatch(typeof(Alert_LifeThreateningHediff), "get_SickPawns")]
+    static class Patch_Alert_LifeThreateningHediff
+    {
+        static void Postfix(ref List<Pawn> __result)
+        {
+            List<Pawn> tmpList = __result.ListFullCopy();
+            for (int i = 0; i < __result.Count; i++)
+            {
+                if (__result[i].guest is Pawn_GuestTracker guestTracker && guestTracker.interactionMode == PrisonerInteractionModeDefOf.HemogenFarm)
+                {
+                    tmpList.Remove(__result[i]);  
+                }
+            }
+            __result = tmpList;
+        }
+    }
+}
